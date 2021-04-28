f84fe486f727377ece38d7cee9b5befc174cbe96
dadpavlo <dad.pavlo@gmail.com>
2021-04-28 23:55:51 +0300

    added car

--- src/Main.java
+++ src/Main.java
@@ -1,43 +1,76 @@
+import java.lang.reflect.Array;
+
 public class Main {
     public static void main(String args []) {
-        TankDestroyer FV4005 = new TankDestroyer("FV4005 STAGE II", "FV4005 STAGE II", "FV4005 STAGE II");
-        FV4005.shoot();
-        FV4005.tankDestroy();
+        TankDestroyer FV4005 = new TankDestroyer("183 MM L4", "FV4005 STAGE II", 1850, 1750);
+        FV4005.getInfo();
+        FV4005.drive();
+        Car kiaRio = new Car("Kia Rio", 2000);
+        kiaRio.stop();
+
     }
 }
 
-class tank implements ActionTank{
-    private String track, turret, gun;
+class Tank implements machine{
+    private String gun, name;
+    private Integer headPoint, damage;
 
-    public tank(String track, String turret, String gun) {
-        this.track = track;
-        this.turret = turret;
+    public Tank(String gun, String name, Integer headPoint, Integer damage) {
         this.gun = gun;
+        this.name = name;
+        this.headPoint = headPoint;
+        this.damage = damage;
     }
 
     public void shoot() {
         System.out.println("The tank fired a shot");
     }
-
-    public void drive() {
-        System.out.println("The tank started moving");
+    public void getInfo()  {
+        System.out.println("Name of the tank : " + name +"\n"
+                            + "his gun : " + gun + "\n");
     }
 
 
-}
+    @Override
+    public void drive() {
+        System.out.println(name + " started moving");
+    }
 
-interface ActionTank {
-    void shoot();
+    @Override
+    public void stop() {
+        System.out.println(name + " stopped moving");
+    }
+}
+interface machine {
     void drive();
-
+    void stop();
 }
 
-class TankDestroyer extends tank {
+class TankDestroyer extends Tank {
 
     public void tankDestroy() {
         System.out.println("tank destroy");
     }
-    public TankDestroyer(String track, String turret, String gun) {
-        super(track, turret, gun);
+    public TankDestroyer(String gun, String name, Integer headPoint, Integer damage) {
+        super(gun, name, headPoint, damage);
+    }
+}
+class Car implements machine {
+    private String name;
+    private Integer yearOfCreation;
+
+    public Car(String name, Integer yearOfCreation) {
+        this.name = name;
+        this.yearOfCreation = yearOfCreation;
+    }
+
+    @Override
+    public void drive() {
+        System.out.println(name + " started moving");
+    }
+
+    @Override
+    public void stop() {
+        System.out.println(name + " stopped of moving");
     }
 }
